f0886dd3e753574d20eb3846634a1e995dfb14e3
Paola Tatiana Bernal R <tec_paolab@confamiliares.com>
2024-11-14 16:14:45 -0500

    ruta archivo ftp prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 const packageJson = require('../../package.json');
 
 const HOST = 'https://3voixiptij.execute-api.us-east-1.amazonaws.com/PD';
+const RUTAS_ARCHIVOS = 'https://alojamiento.confa.co/guardarArchivosRest/guardarArchivo/metodo1';
 
 export const environment = {
   API_PUBLIC: HOST + '/',
@@ -8,4 +9,5 @@ export const environment = {
   context: 'master',
   version: packageJson.version,
   minutesInactive: 15,
+  ruta_archivos_ws: RUTAS_ARCHIVOS,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ const packageJson = require('../../package.json');
 const HOST = 'https://m7co9zv7kk.execute-api.us-east-1.amazonaws.com/DEV';
 //const HOST = 'https://3voixiptij.execute-api.us-east-1.amazonaws.com/PD';
 //PRUEBAS
-//const RUTAS_ARCHIVOS = 'https://app.confa.co:8687/guardarArchivosRest/guardarArchivo/metodo1';
+const RUTAS_ARCHIVOS = 'https://app.confa.co:8687/guardarArchivosRest/guardarArchivo/metodo1';
 //PRODUCCION
 //const RUTAS_ARCHIVOS = 'https://alojamiento.confa.co/guardarArchivosRest/guardarArchivo/metodo1';
 export const environment = {
@@ -15,5 +15,5 @@ export const environment = {
   // context: 'production',
   version: packageJson.version,
   minutesInactive: 15,
-  // ruta_archivos_ws: RUTAS_ARCHIVOS,
+  ruta_archivos_ws: RUTAS_ARCHIVOS,
 };
